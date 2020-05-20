2c93097d53178e445edc1b33d6845dc4c1b2ef1b
Arlei F. Farnetani Junior <farneetani@gmail.com>
2020-05-20 14:07:02 -0300

    feat: implemented other example

--- concurrency/1/main.go
+++ concurrency/1/main.go
@@ -7,10 +7,14 @@ import (
 )
 
 func main() {
-	go runProcess("P1", 20)
-	go runProcess("P2", 20)
+	runProcess("P1", 20)
+	runProcess("P2", 20)
+
+	go runProcess("P1", 20)  // O fato de colocar o go na frente significa que irão rodar em paralelo
+	go runProcess("P2", 20)  // O fato de colocar o go na frente significa que irão rodar em paralelo
 
 	var s string
+	// Isso abaixo serve para pegar os resultados em tempo de execução
 	fmt.Scanln(&s)
 }
 
